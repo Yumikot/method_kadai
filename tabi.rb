24bb9216d36481186ec5ecced8492e13659ae409
Yumiko Tezuka <ytezuka5@gmail.com>
2020-09-19 13:39:57 +0900

    travel_choice修正中

--- tabi.rb
+++ tabi.rb
@@ -1,3 +1,13 @@
+
+def plan(travels)
+   travels.each.with_index(1) do |travel,i| 
+      puts "#{i}.#{travel[:name]}¥#{travel[:price]}"
+   end
+end
+
+
+
+
 puts "旅行プランを選択して下さい"
 
 travels = [
@@ -6,33 +16,29 @@ travels = [
             {name:"九州旅行", price:15000}
         ]
 
-
-travels.each.with_index(1) do |travel,i| 
-    puts "#{i}.#{travel[:name]}¥#{travel[:price]}"
-end
+ plan(travels)
 
 puts "プランを選択 >"
 
-travels_selected_num = gets.to_i
-travels_menu = travels[travels_selected_num - 1]
-   
-    
- puts "#{travels_menu[:name]}ですね, 何人で行きますか？"
+def travels_choice(travels)
+   travels_selected_num = gets.to_i
+   travels_menu = travels[travels_selected_num - 1]    
+   puts "#{travels_menu[:name]}ですね, 何人で行きますか？"  
+end
+
+travels_choice(travels)
 
  puts "人数を入力 >"
- people_selected_num = gets.to_i
+ 
+   people_selected_num = gets.to_i
 
- if people_selected_num >= 5
-    puts "5人以上なので10%割引になります"
-    sale_price = travels_menu[:price]-travels_menu[:price]/10
-    sum = sale_price*people_selected_num 
-    puts "合計金額:¥#{sum}" 
-    
- else
-    sum = travels_menu[:price]*people_selected_num 
-    puts "合計金額:¥#{sum}" 
-   
- end
-
-       
-    
+   if people_selected_num >= 5
+      sale_price = travels_menu[:price]-travels_menu[:price]/10
+      sum = sale_price*people_selected_num     
+      puts "5人以上なので10%割引になります"
+      puts "合計金額:¥#{sum}" 
+   else
+      sum = travels_menu[:price]*people_selected_num  
+      puts "合計金額:¥#{sum}" 
+   end
+  
